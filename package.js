795649621d72c5720f42cfcb57921550ae9b54bc
Phuoc Do <dnprock@gmail.com>
2013-08-15 18:57:43 -0700

    Add slick grid css

--- package.js
+++ package.js
@@ -5,6 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
   api.use('jquery', 'client');
   api.add_files([
+      'lib/SlickGrid/slick.grid.css',
       'lib/jquery.event.drag.js',
       'lib/jquery.event.drop.js',
       'lib/SlickGrid/slick.core.js',
