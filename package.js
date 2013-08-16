89deb0276a0816b73f663862e3c2b21220e4dd33
Phuoc Do <dnprock@gmail.com>
2013-08-15 18:46:29 -0700

    Add slick core javascript

--- package.js
+++ package.js
@@ -5,9 +5,11 @@ Package.describe({
 Package.on_use(function (api) {
   api.use('jquery', 'client');
   api.add_files([
-    'lib/jquery.event.drag.js',
-    'lib/jquery.event.drop.js',
-    'lib/SlickGrid/slick.*.js']
+      'lib/jquery.event.drag.js',
+      'lib/jquery.event.drop.js',
+      'lib/SlickGrid/slick.core.js',
+      'lib/SlickGrid/slick.grid.js',
+    ]
     ,'client'
   );
 });
