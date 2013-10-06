07ee9473888afba03b60a01f0e48a8a83eb47620
Phuoc Do <dnprock@gmail.com>
2013-10-06 16:20:50 -0700

    Add missing header-columns-bg.gif

--- package.js
+++ package.js
@@ -15,7 +15,8 @@ Package.on_use(function (api) {
       'lib/SlickGrid/slick.formatters.js',
       'lib/SlickGrid/slick.groupitemmetadataprovider.js',
       'lib/SlickGrid/slick.grid.js',
-      'lib/SlickGrid/slick.remotemodel.js'
+      'lib/SlickGrid/slick.remotemodel.js',
+      'lib/SlickGrid/images/header-columns-bg.gif'
     ]
     ,'client'
   );
